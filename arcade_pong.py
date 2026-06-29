69c6fdfee5ac7050c0f3688f54b9e1422a0aa75d
ALEJO MARTINEZ <145155973+AlejoMartinez1995@users.noreply.github.com>
2026-06-28 21:21:25 -0300

    Simplify Pong game description
    Removed unnecessary details from the game description.

--- arcade_pong.py
+++ arcade_pong.py
@@ -1,8 +1,4 @@
-'''Reproduce el clásico juego de arcade Pong. Para ello puedes usar el módulo "turtle" para
-crear los componentes del juego y detectar las colisiones de la pelota con las paletas de los
-jugadores.También puedes definir una serie de asignaciones de teclas para establecer los
-controles del usuario para las paletas de los jugadores izquierda y derecha.
-El juego debe incluir una pelota que se mueva por la pantalla, rebotando en las paredes y las paletas de los jugadores. 
+'''Reproduce el clásico juego de arcade Pong. 
 El objetivo del juego es que cada jugador intente evitar que la pelota pase más allá de su paleta, anotando puntos cuando la pelota pasa la paleta del oponente.'''
 
 import turtle
